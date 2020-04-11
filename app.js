eaec3ab393889c2cb6f229180db328e7e5a009b7
Safnas <iamsafnas@gmail.com>
2020-04-11 23:46:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+//const FacebookStrategy = require('passport-facebook').Strategy
+const findOrCreate = require('mongoose-findorcreate')
 
 const app = express();
 
@@ -9,6 +15,18 @@ require('dotenv').config()
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365
+    }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const uri = process.env.ATLAS_URI_TODO;
 
@@ -27,12 +45,12 @@ const itemsSchema = mongoose.Schema({
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-    title: "Welcome to Todo List App",
+    title: "Welcome to Easy ToDo, The simplest one around 😉",
     checked: false
 })
 
 const item2 = new Item({
-    title: "Add New Item and Hit the + button to add a ToDo",
+    title: "Add New Item and Hit the ➕ button to add a ToDo",
     checked: false
 })
 
@@ -42,12 +60,12 @@ const item3 = new Item({
 })
 
 const item4 = new Item({
-    title: "Hit x button to delete an item",
+    title: "Hit ❌ button to delete an item",
     checked: false
 })
 
 const item5 = new Item({
-    title: "Delete all to see the tips again ;)",
+    title: "Delete all to see the tips again 😉",
     checked: false
 })
 
@@ -58,27 +76,173 @@ const item6 = new Item({
 
 const defaultItems = [item1, item2, item3, item4, item5, item6];
 
-app.get('/', (req, res) => {
-    Item.find()
-        .catch((err) => {
-            console.log(err)
-        }).then(foundItems => {
-            if (foundItems.length === 0) {
-                Item.insertMany(defaultItems)
-                    .catch((err) => {
-                        console.log(err)
-                    }).then(res.redirect('/'))
 
-            } else {
-                res.render("list", {
-                    listTitle: "EasyToDo", newListItems: foundItems
-                })
-
-            }
-        })
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    googleId: String
 })
 
-app.post('/', (req, res) => {
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
+
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy())
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user)
+    })
+})
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/secrets'
+},
+    (accessToken, refreshToken, profile, cb) => {
+        User.findOrCreate({ googleId: profile.id, username: profile.displayName }, (err, user) => {
+            return cb(err, user)
+        })
+    }
+))
+
+app.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.redirect('/notes')
+    } else {
+        res.render('home');
+    }
+})
+
+app.get('/notes', (req, res) => {
+    if (req.isAuthenticated()) {
+
+        Item.find()
+            .catch((err) => {
+                console.log(err)
+            }).then(foundItems => {
+                if (foundItems.length === 0) {
+                    Item.insertMany(defaultItems)
+                        .catch((err) => {
+                            console.log(err)
+                        }).then(res.redirect('/notes'))
+
+                } else {
+                    res.render("list", { newListItems: foundItems, username: req.user.username })
+                }
+            })
+    } else {
+        res.redirect('/login')
+    }
+})
+
+app.get('/login', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.redirect('/notes')
+    } else {
+        res.render('login')
+    }
+})
+
+app.get('/register', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.redirect('/notes')
+    } else {
+        res.render('register', { error: '', username: '', email: '' })
+    }
+})
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }))
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/notes');
+    });
+
+function validate(body) {
+    if (body.username === body.password) {
+        return 'username and password cannot be same'
+    } else if (body.password !== body.password1) {
+        return 'Please enter same password'
+    }
+
+    re = /[0-9]/;
+    if (!re.test(body.password)) {
+        return 'password must contain atleast a number (0-9)!'
+    }
+
+    re = /[a-z]/;
+    if (!re.test(body.password)) {
+        return 'password must contain atleast a lower case letter (a-z)!'
+    }
+
+    re = /[A-Z]/;
+    if (!re.test(body.password)) {
+        return 'password must contain atleast an upper case letter (A-Z)!'
+    }
+
+    return 'true';
+}
+
+app.post('/register', (req, res) => {
+    console.log(req.body)
+    let validationError = validate(req.body)
+    if (validationError !== 'true') {
+        res.render('register', { error: validationError, username: req.body.username, email: req.body.email })
+    } else {
+        User.register({ username: req.body.username, email: req.body.email }, req.body.password, (err, user) => {
+            if (err) {
+                res.render('register', { error: err.message, username: req.body.username, email: req.body.email })
+            } else {
+                passport.authenticate('local')(
+                    req, res, () => {
+                        res.redirect('/notes')
+                    }
+                )
+            }
+        })
+    }
+})
+
+
+app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+app.post('/login', (req, res) => {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err)
+
+        } else {
+            passport.authenticate('local', { failureRedirect: '/register' })(
+                req, res, () => {
+                    res.redirect('/notes')
+                }
+            )
+        }
+    })
+
+})
+
+
+app.post('/notes', (req, res) => {
     const itemTitle = req.body.newItem;
 
     const item = new Item({
@@ -89,7 +253,7 @@ app.post('/', (req, res) => {
     item.save()
         .catch((err) => {
             console.log(err)
-        }).then(res.redirect('/'))
+        }).then(res.redirect('/notes'))
 })
 
 app.post("/delete", (req, res) => {
@@ -97,7 +261,7 @@ app.post("/delete", (req, res) => {
     Item.findByIdAndRemove(deleteItem)
         .catch((err) => {
             console.log(err)
-        }).then(res.redirect('/'))
+        }).then(res.redirect('/notes'))
 })
 
 app.post("/update", (req, res) => {
@@ -108,7 +272,7 @@ app.post("/update", (req, res) => {
         .catch((err) => {
             console.log(err)
         })
-        .then(res.redirect('/'))
+        .then(res.redirect('/notes'))
 })
 
 app.listen(process.env.PORT || 3000, () => {
