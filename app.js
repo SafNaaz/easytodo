4234ee2b01673144e25158d85b4e41654f4a2243
Safnas <iamsafnas@gmail.com>
2020-04-14 19:42:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -252,7 +252,7 @@ app.get('/auth/google',
 //         res.redirect('/notes');
 //     });
 
-app.get('/auth/google/todo',
+app.get('/auth/google/easytodo',
     passport.authenticate('google'),
     (err, req, res, next) => {
         if (err) {
